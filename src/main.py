e33f9ce30adf3b867ea118e16a067fc43066aa74
Malcolm Miller <malcolmjmil@gmail.com>
2025-07-22 00:26:34 -0400

    removed some prints

--- src/main.py
+++ src/main.py
@@ -47,9 +47,9 @@ def handle_github_comment_event(issue: Issue, issue_comment_id: int) -> None:
 
     if ai_enhanced_comment is None:
         return
-    print(ai_enhanced_comment)
+
     userStoryEval = UserStoryEvalResponse.from_markdown(ai_enhanced_comment)
-    print(f"\n {userStoryEval.to_markdown()}")
+
     print(f"\n {userStoryEval.suggestions.title}")
     print(f"\n {userStoryEval.suggestions.description}")
     print(f"\n {userStoryEval.suggestions.acceptance_criteria}")
